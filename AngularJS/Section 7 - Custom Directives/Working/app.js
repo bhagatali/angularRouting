4cb0e6494b00680801390bba5b2c6c24e82c6fcb
bhagatali <bhagat.ali@gmail.com>
2016-01-11 16:17:22 -0500

    Custom Directives

--- AngularJS/Section 7 - Custom Directives/Working/app.js
+++ AngularJS/Section 7 - Custom Directives/Working/app.js
@@ -34,15 +34,43 @@ myApp.controller("myController",["$scope","$log","myCustomService",function($sco
        myCustomService.name = $scope.name; 
     });
 
-    $scope.bank = {
-        name:"Scotiabank",   
-        motto:"Why We Bank",
+    $scope.bank={
+        name:"Bank of Jupiter",   
+        motto:"We are broke",
+        message:"Show me the money!",
+        address:"786 Plymoth St",
+        city:"Dark Moon",
+        state:"Alpha Beta Gamma",
+        zip:"67581"
+};
+    
+    $scope.bankPlanet = [{
+        name:"Bank of Mercury",   
+        motto:"Why We Bank?",
         message:"They keep us focused on what matters most. The answer is clear: our customers.",
         address:"786 Main St",
         city:"Madivala",
         state:"Tupperware",
         zip:"110"
-    };
+    },
+    {
+        name:"Bank of Venus",   
+        motto:"The V in the Venus is us.",
+        message:"Customer is always right.",
+        address:"654 Martian Rock",
+        city:"Waterhole",
+        state:"Bedrock",
+        zip:"799"
+    },
+    {
+        name:"Bank of Earth",   
+        motto:"We are the bank the dinosaurs used.",
+        message:"Make Millions!",
+        address:"098 Taco Street",
+        city:"Hot Sauce",
+        state:"Burrito",
+        zip:"09867"
+    }];
         
     $scope.ohWhatFun = function(bank){
         return bank.address + ', ' + bank.city + ', ' + bank.state + ', ' + bank.zip;
