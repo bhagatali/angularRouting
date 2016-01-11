99f3485b01f322df0704df512ce8de2edb832cef
bhagatali <bhagat.ali@gmail.com>
2016-01-10 21:53:24 -0500

    Custom Services

--- AngularJS/Section 6 - Custom Services/Working/app.js
+++ AngularJS/Section 6 - Custom Services/Working/app.js
@@ -1,5 +1,14 @@
 var myApp = angular.module("myApp",["ngRoute"]);
 
+myApp.service("myCustomService",function(){
+    var self = this;
+    
+    this.name = "Rolling Stones";
+    this.nameLength =  function(){
+        return self.name.length;
+    }    
+});
+
 myApp.config(function($routeProvider){
     
     $routeProvider
@@ -18,11 +27,23 @@ myApp.config(function($routeProvider){
     })
 });
 
-myApp.controller("myController",["$scope",function($scope){
-    $scope.name = "Ayaan";
+myApp.controller("myController",["$scope","$log","myCustomService",function($scope,$log,myCustomService){
+    $scope.name = myCustomService.name;
+    
+    $scope.$watch("name",function(){
+       myCustomService.name = $scope.name; 
+    });
+    
+    $log.log(myCustomService.name);
+    $log.log(myCustomService.nameLength());
 }]);
 
-myApp.controller("secondController",["$scope","$routeParams",function($scope,$routeParams){
+myApp.controller("secondController",["$scope","$log","$routeParams","myCustomService",function($scope,$log,$routeParams,myCustomService){
     $scope.name = "Minal";
     $scope.num = $routeParams.number || 1;
+    
+    $scope.name = myCustomService.name;
+    
+    $log.log(myCustomService.name);
+    $log.log(myCustomService.nameLength());
 }]);
